c4d3318063d45352620101ecad28ecf5a4b7b732
griggheo <grig@gheorghiu.net>
2019-09-01 19:42:38 -0700

    helper scripts and modified docker-compose files for flask-by-example app

--- flask-by-example/config.py
+++ flask-by-example/config.py
@@ -6,7 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = 'Uu8Aexei7ailiefee9ae'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
